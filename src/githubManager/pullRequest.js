b91164e8dd41d5dad711f41f125f58550ede7f37
lokesh-selvam10 <lokeshselvam10@gmail.com>
2025-04-27 20:39:53 +0530

    feature: add POST /users endpoint

--- src/githubManager/pullRequest.js
+++ src/githubManager/pullRequest.js
@@ -5,7 +5,9 @@ dotenv.config();
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 export async function createPullRequest(branchName, title, body) {
-  const [owner, repo] = process.env.GITHUB_REPO.split("/");
+  //   const [ repo] = process.env.GITHUB_REPO.split("/");
+  const owner = process.env.GITHUB_OWNER;
+  const repo = process.env.GITHUB_REPO;
 
   await octokit.pulls.create({
     owner,
